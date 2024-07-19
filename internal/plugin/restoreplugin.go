af6667b4cd4bda53e02d3a54998e7feaaa67fdf0
Winness R. <winness.rakotozafy@outlook.fr>
2024-07-20 00:04:27 +0200

    feat: add base template plugin example

---
+++ internal/plugin/restoreplugin.go
@@ -0,0 +1,49 @@
+/*
+Copyright 2018, 2019 the Velero contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package plugin
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/vmware-tanzu/velero/pkg/plugin/velero"
+)
+
+// RestorePlugin is a restore item action plugin for Velero
+type RestorePlugin struct {
+	log logrus.FieldLogger
+}
+
+// NewRestorePlugin instantiates a RestorePlugin.
+func NewRestorePlugin(log logrus.FieldLogger) *RestorePlugin {
+	return &RestorePlugin{log: log}
+}
+
+// AppliesTo returns information about which resources this action should be invoked for.
+// The IncludedResources and ExcludedResources slices can include both resources
+// and resources with group names. These work: "ingresses", "ingresses.extensions".
+// A RestoreItemAction's Execute function will only be invoked on items that match the returned
+// selector. A zero-valued ResourceSelector matches all resources.
+func (p *RestorePlugin) AppliesTo() (velero.ResourceSelector, error) {
+	return velero.ResourceSelector{}, nil
+}
+
+// Execute allows the RestorePlugin to perform arbitrary logic with the item being restored,
+// in this case, setting a custom annotation on the item being restored.
+func (p *RestorePlugin) Execute(input *velero.RestoreItemActionExecuteInput) (*velero.RestoreItemActionExecuteOutput, error) {
+	p.log.Infof("Restoring item with GroupVersionKind: %s", input.Item.GetObjectKind().GroupVersionKind().String())
+
+	return velero.NewRestoreItemActionExecuteOutput(input.Item), nil
+}
