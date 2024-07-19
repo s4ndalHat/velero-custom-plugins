7ec1ec6da0fabb6d38da8a923f62ef505f6d1e72
Winness R. <winness.rakotozafy@outlook.fr>
2024-07-20 00:19:22 +0200

    feat: add test placeholder

---
+++ internal/plugin/replace_pattern_action_test.go
@@ -0,0 +1,12 @@
+// package plugin
+
+// import (
+// 	"encoding/json"
+// 	"github.com/sirupsen/logrus"
+// 	"github.com/vmware-tanzu/velero/pkg/plugin/velero"
+// 	"k8s.io/apimachinery/pkg/runtime"
+// 	"strings"
+// 	"testing"
+// )
+
+// func TestReplacePatternAction()
